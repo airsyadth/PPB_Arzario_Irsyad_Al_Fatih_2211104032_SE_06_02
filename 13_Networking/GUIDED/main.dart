e7421af5fa6bcb8d6b452d8464787a942141a61c
Arzario Irsyad Al Fatih <136824245+airsyadth@users.noreply.github.com>
2024-12-13 21:55:20 +0700

    Add files via upload

---
+++ 13_Networking/GUIDED/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:praktikum_13/viewmodel/counter_controller.dart';
+import 'package:praktikum_13/view/detail_page.dart';
+import 'package:praktikum_13/view/my_home_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  final CounterControler controller = Get.put(CounterControler());
+
+  @override
+
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+       getPages: [
+        GetPage(
+          name: '/', 
+          page: () => MyHomePage(title: 'Belajar GetX'),
+        ),
+        GetPage(
+          name: '/Detail', 
+          page: () => const DetailPage(),
+        ),
+       ],
+    );
+  }
+}
